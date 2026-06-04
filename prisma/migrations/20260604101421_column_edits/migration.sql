fb64a9c3ebff2cfd428b349a8cc40a601f87a7eb
Vijay <vijay.r@gmail.com>
2026-06-04 15:47:33 +0530

    column changes

---
+++ prisma/migrations/20260604101421_column_edits/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "tbl_expression_of_interest" ALTER COLUMN "chr_document_status" SET DEFAULT 'N';
+
+-- AlterTable
+ALTER TABLE "tbl_purchase_request" ALTER COLUMN "chr_document_status" SET DEFAULT 'N';
+
+-- AlterTable
+ALTER TABLE "tbl_vendor" ALTER COLUMN "chr_document_status" SET DEFAULT 'N';
