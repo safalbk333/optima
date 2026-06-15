392a92b4360fcd9099ff397e8fb1e2ab5c4b78bd
safal.b <safal.b@gnxsolutions.in>
2026-06-15 17:52:44 +0530

    dynamic db

--- libs/database/prisma-service.ts
+++ libs/database/prisma-service.ts
@@ -7,26 +7,37 @@ import {
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: `${process.env.DATABASE_URL}?schema=econet`,
-        },
-      },
-    });
-  }
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly clients = new Map<string, PrismaClient>();
 
+  async getClient(schema: string): Promise<PrismaClient> {
+    if (!this.clients.has(schema)) {
+      const client = new PrismaClient({
+        datasources: {
+          db: {
+            url: `${process.env.DATABASE_URL}?schema=${schema}`,
+          },
+        },
+      });
+
+      await client.$connect();
+
+      console.log(`Prisma connected for schema: ${schema}`);
+
+      this.clients.set(schema, client);
+    }
+
+    return this.clients.get(schema)!;
+  }
   async onModuleInit() {
-    await this.$connect();
     console.log('Master Prisma connected');
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    for (const client of this.clients.values()) {
+      await client.$disconnect();
+    }
   }
 }
+
+
